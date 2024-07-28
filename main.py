fdbb94259cb54df75fec31fcf2b0c2d6333ca09e
Joannes <joannesribeiro_@outlook.com>
2024-07-28 19:32:42 -0300

    Fix Title 'Tranform Data'

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from src.transform_data import *
 url, headers = load_json("request_data.json")
 response = request_wanted_fbi(url, headers)
 data = extract_data_wanted(response)
-info_data = ["title", "age_max", "age_min", "details"]
+info_data = ["title", "age_max", "age_min", "age_range", "location", "details", "subjects"]
 extracted_data = iteration_data_wanted(data, info_data)
 
-# Transform
-columns_name = ["title", "age_max", "age_min", "details"]
+# Transform Data
+columns_name = ["title", "age_max", "age_min", "age_range", "location", "details", "subjects"]
 transformed_data = transform_data_for_columns(extracted_data, columns_name)
 data_dict = columns_with_values(columns_name, transformed_data)
 df = create_dataframe(data_dict)
@@ -17,3 +17,5 @@ df = transform_values_for_nan(df, "Null")
 df = transform_values_str_with_replace(df, "details", ["<p>", "</p>"], "")
 df = change_type_values(df, "age_max")
 df = change_type_values(df, "age_min")
+
+print(df.head())
